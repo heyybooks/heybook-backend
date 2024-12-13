a9c461d5806b789b398a6be44d085f992e35bfd1
Özcan ÇAVGA <cavga1997@gmail.com>
2024-12-13 15:25:36 +0300

    EntityFramework design packet added.

--- src/modules/Books/Books.DataAccess/EntityFramework/HeybooksContext.cs
+++ src/modules/Books/Books.DataAccess/EntityFramework/HeybooksContext.cs
@@ -7,17 +7,18 @@ namespace Core.DataAccess.EntityFramework
     public class HeybooksContext : DbContext
     {
         // Veritabanı bağlantı ayarları burada yapılır
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             {
-                // PostgreSQL bağlantı dizesi
-                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=hey-books;Username=postgres;Password=admin123");
+                if (!optionsBuilder.IsConfigured)
+                {
+                    // PostgreSQL bağlantı dizesi
+                    optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=hey-books;Username=postgres;Password=admin123");
+                }
             }
-        }
 
         // DbSet tanımları (örneğin Book ve BookImage tabloları)
         public DbSet<Book> Books { get; set; }
         public DbSet<BookImage> BookImages { get; set; }
     }
 }
+        
