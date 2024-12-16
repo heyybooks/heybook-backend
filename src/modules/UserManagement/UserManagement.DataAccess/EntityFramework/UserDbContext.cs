0d2f8a33d2d8a2db2bcb9d3d46428cfebeac5220
busra2 <busra.kemter@outlook.com>
2024-12-16 19:29:52 +0300

    feat:Add migration for UserManagement table

---
+++ src/modules/UserManagement/UserManagement.DataAccess/EntityFramework/UserDbContext.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Entity.Concrete;
+
+
+
+namespace UserManagement.DataAccess.EntityFramework
+{
+    public class UserDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    // PostgreSQL bağlantı dizesi
+                    optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=hey-books;Username=postgres;Password=admin123");
+                }
+            }
+        public DbSet<User> Users { get; set; }   
+
+        
+    }
+}
