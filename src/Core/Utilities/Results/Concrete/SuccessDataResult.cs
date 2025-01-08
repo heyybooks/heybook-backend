40b233e3c9f90269c1bd2fda25141d8fb6a4c41f
Özcan ÇAVGA <cavga1997@gmail.com>
2025-01-08 17:28:01 +0300

    refactor: BooksController, result systems, BookManager edited and Bookfactory added.

--- src/Core/Utilities/Results/Concrete/SuccessDataResult.cs
+++ src/Core/Utilities/Results/Concrete/SuccessDataResult.cs
@@ -1,27 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Utilities.Results.Concrete;
 
 namespace Core.Utilities.Results.Concrete
 {
     public class SuccessDataResult<T> : DataResult<T>
     {
-        public SuccessDataResult(T data, string message):base(data, true, message)
+        public SuccessDataResult(T data, string message) : base(data, data != null, message)
         {
-
         }
 
-        public SuccessDataResult(T data) : base(data, true)
+        public SuccessDataResult(T data) : base(data, data != null)
         {
-
         }
 
-        public SuccessDataResult(string message):base(default, true, message)
+        public SuccessDataResult(string message) : base(default, false, message)
         {
-
         }
-
     }
 }
